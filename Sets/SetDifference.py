a8eda2c12f391cf56e447bdd31c18d72791b30d9
Nagendra-NR <nr_nagendra@yahoo.com>
2017-10-12 16:30:04 +0530

    Create SetDifference.py

---
+++ Sets/SetDifference.py
@@ -0,0 +1,14 @@
+#first line = number of students who subscribed to english news paper
+n = int(input())
+
+#secondline = roll numbers of english students
+n_set = set(map(int, input().split()))
+
+#third line = number of students who subscribed to french news paper
+b = int(input())
+
+#fourth line = roll numbers of french students
+b_set = set(map(int, input().split()))
+
+
+print (len( n_set.difference(b_set)) )
